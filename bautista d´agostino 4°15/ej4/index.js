8b1e0f64907ab8924e2f1ca92976d81204baa973
ELFREDDYS300 <elfreddys300@gmail.com>
2024-03-25 15:11:30 -0300

    tp7

---
+++ bautista d´agostino 4°15/ej4/index.js
@@ -0,0 +1,21 @@
+function ejercicio4(){
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+    'E', 'T'];
+    var numero = (prompt("Ingrese un numero de DNI"));
+
+    var letra;
+    var resultado;
+
+    
+    if(numero<0||numero>99999999){
+        console.log("Ingrese un numero valido");
+    }
+else{
+    numero/23 == resultado;
+    letras[resultado];
+console.log(letras);
+
+}
+
+
+}
