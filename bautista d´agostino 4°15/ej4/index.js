84319e034439b345b2705ba851886491d1678a90
ELFREDDYS300 <elfreddys300@gmail.com>
2024-03-25 16:57:26 -0300

    Update index.js

--- bautista d´agostino 4°15/ej4/index.js
+++ bautista d´agostino 4°15/ej4/index.js
@@ -1,21 +1,29 @@
-function ejercicio4(){
-    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
-    'E', 'T'];
-    var numero = (prompt("Ingrese un numero de DNI"));
 
-    var letra;
-    var resultado;
-
+    function ejercicio4() {
+        let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K','E', 'T'];
+        let dni = document.querySelector("#dni").value;
+        let letraUser = document.querySelector("#letra");
+        let Nletra = dni % 23;
+       
+        console.log(letras[Nletra]);
     
-    if(numero<0||numero>99999999){
-        console.log("Ingrese un numero valido");
+        if (letra == letraUser) {
+            console.log("Las letras son iguales");
+        } else {
+            console.log("Las letra ingresada es incorrecta");
+        }
+    
+        if (dni > 0 && dni < 99999999) {
+            
+            let letra = letras[Nletra];
+        
+            console.log("la letra de tu DNI es:  "+ letra);
+    
+        
+        } else {
+            console.log("El DNI es incorrecto");
+        }
+    
+    
     }
-else{
-    numero/23 == resultado;
-    letras[resultado];
-console.log(letras);
-
-}
-
-
-}
+    
