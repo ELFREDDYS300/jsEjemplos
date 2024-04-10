30a5cb97a427a8dd70f26f919e2fb8d00f2c62ab
ELFREDDYS300 <163897171+ELFREDDYS300@users.noreply.github.com>
2024-04-10 14:01:00 -0300

    Add files via upload

---
+++ bautista d´agostino 4°15/ej8/index.js
@@ -0,0 +1,16 @@
+function ejercicio8(){
+
+    function esPalindromo(cadena){
+        cadena = cadena.toLowerCase().replace(/  /g, '');
+
+        return cadena === cadena.split('').reverse().join('');
+    }
+
+    var texto = "La ruta nos aporto otro paso natural";
+    if (esPalindromo(texto)){
+        console.log("La cadena es un palindromo");
+    } else {
+        console.log("La cadena no es un palindromo");
+    
+    }
+}
