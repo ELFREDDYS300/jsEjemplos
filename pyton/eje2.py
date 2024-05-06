ee13e46d986f50371e0380c2b9da08a33fe13fe1
Bruno <brunorojass2212@gmail.com>
2024-05-06 17:30:09 -0300

    Create eje2.py

---
+++ pyton/eje2.py
@@ -0,0 +1,13 @@
+
+#Leer 4 números enteros, calcular la suma e informar el resultado.
+def Suma(): 
+    n1 = int(input("Ingrese un numero entero"))
+    n2 = int(input("Ingrese el segundo numero entero"))
+    n3 = int(input("Ingrese el tercer numero entero"))
+    n4 = int(input("Ingrese el ultimo entero mas"))
+
+    resultado = n1 + n2 + n3 + n4
+
+    print("El resultado es", resultado)
+
+Suma()
