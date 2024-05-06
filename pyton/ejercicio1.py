84d6ac2e88ec3332b56d6e67b2567e5e7869554f
Bruno <brunorojass2212@gmail.com>
2024-05-06 16:13:49 -0300

    Create ejercicio1.py

---
+++ pyton/ejercicio1.py
@@ -0,0 +1,8 @@
+#Crear una lista llamado meses y que almacene el nombre de los doce meses del año.
+#Mostrar por pantalla los doce nombres utilizando la función print()
+
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+for MES in meses:
+    
+ print(meses[MES])
